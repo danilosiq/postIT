280d18ff3f1e82d7945f6331b6d2f5a899d1a6bd
Danilo Siqueira <danilodsiqueira@outlook.com>
2024-02-26 16:45:28 -0300

    d

--- front/src/utils/config.jsx
+++ front/src/utils/config.jsx
@@ -1,5 +1,5 @@
-export const api = "http://localhost:5000/api";
-export const uploads = "http://localhost:5000/uploads";
+export const api = import.meta.env.API_URL;
+export const uploads = import.meta.env.UPLOADS_URL;
 
 export const requestConfig = (method, data, token = null, image = null) => {
   let config;
